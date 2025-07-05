48a1b47ba4e56fecf244ebc06de2ff2918442271
kenny.chuang <kenny.chuang@innolux.com>
2025-07-06 02:16:17 +0800

    fix-Audit

--- api_demo/src/main/java/com/example/api_demo/Model/Audit.java
+++ api_demo/src/main/java/com/example/api_demo/Model/Audit.java
@@ -2,15 +2,18 @@ package com.example.api_demo.Model;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class Audit {
     @CreatedDate
     @Column(name = "created_date", updatable = false)
